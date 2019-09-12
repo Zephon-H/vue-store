6c63efb7e56ff62a0b5efc860e6f0f3e6c76a122
Zephon-H <1528498238@qq.com>
2019-09-13 00:54:48 +0800

    add navbar

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="nav-bar">
+    <div class="left">
+      <slot name="left"></slot>
+    </div>
+    <div class="center">
+      <slot name="center"></slot>
+    </div>
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'NavBar',
+  props: [''],
+  data () {
+    return {
+
+    }
+  },
+
+  components: {},
+
+
+}
+
+</script>
+<style>
+.nav-bar {
+  display: flex;
+  height: 44px;
+  line-height: 44px;
+}
+.left,
+.right {
+  width: 60px;
+  text-align: center;
+}
+.center {
+  flex: 1;
+  text-align: center;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>
-    home
+  <div id='home'>
+    <nav-bar class="home-nav">
+      <span slot="left"
+            class="iconfont icon-menu"
+            style="font-size:22px"></span>
+      <div slot="center">
+        <span class="red">JD</span>
+        <i class='gray'>|</i>
+        <span class="iconfont icon-search gray"></span>
+        <input placeholder="华为平板电脑" />
+      </div>
+      <span slot="right">登录</span>
+    </nav-bar>
   </div>
 </template>
 
 <script>
-
+import NavBar from 'components/common/navbar/NavBar.vue'
 export default {
   name: 'Home',
   props: [''],
@@ -13,9 +24,40 @@ export default {
     return {
 
     }
+  },
+  components: {
+    NavBar
   }
 }
 
 </script>
 <style scoped>
+.home-nav {
+  background-color: #c93b22;
+  color: #fff;
+}
+.home-nav div {
+  width: 100%;
+  background-color: #fff;
+  border: none;
+  border-radius: 100px;
+  height: 30px;
+  margin-top: 7px;
+  line-height: 30px;
+}
+
+.home-nav input {
+  border: none;
+  height: 78%;
+  outline: none;
+  /* border-radius: 100px; */
+  /* width: 100%; */
+}
+
+.red {
+  color: red;
+}
+.gray {
+  color: #ccc;
+}
 </style>
